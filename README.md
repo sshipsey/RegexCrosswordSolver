772510d7e5e6a8281444258c4408bc51e5733670
Steve Shipsey <sashipsey@gmail.com>
2016-01-30 17:51:59 -0500

    Committing while this kinda works, so there is proof that it happened.

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ https://regexcrossword.com solver in Python
 Selenium: https://pypi.python.org/pypi/selenium
 BeautifulSoup4: https://pypi.python.org/pypi/beautifulsoup4
 PhantomJS: https://www.npmjs.com/package/phantomjs
-rstr: https://pypi.python.org/pypi/rstr/2.1.3
--- main.py
+++ main.py
@@ -1,21 +1,50 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from bs4.element import Tag
-import rstr
+import itertools
 import re
 
-site = 'https://regexcrossword.com/challenges/tutorial/puzzles/1'
+class Solver:
+    site = 'https://regexcrossword.com/challenges/tutorial/puzzles/1'
+    characterSet = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
 
-browser = webdriver.PhantomJS()
-browser.get(site)
-pageSoup = BeautifulSoup(browser.page_source, "html.parser")
-vertClueSoup = pageSoup.find_all(re.compile('th'), class_ = "clue")
-horizClueSoup = pageSoup.find_all(re.compile('div'), class_ = "clue")
+    browser = webdriver.PhantomJS()
+    browser.get(site)
+    pageSoup = BeautifulSoup(browser.page_source, "html.parser")
+    vertClueSoup = pageSoup.find_all(re.compile('th'), class_ = "clue")
+    horizClueSoup = pageSoup.find_all(re.compile('div'), class_ = "clue")
 
+    # Parse strings for horiz and vertical
+    horizClues = [ tag.contents[0] for soup in horizClueSoup for tag in soup.contents if type(tag) == Tag and len(tag.contents) > 0 ]
+    vertClues = [ tag.contents[0] for soup in vertClueSoup for tag in soup.contents if type(tag) == Tag and len(tag.contents) > 0 ]
+
+    def solve(self):
+
+        # For every square in the grid
+        for x, hc in enumerate(self.horizClues):
+            for y, vc in enumerate(self.vertClues):
+                print(self.getVal(x, y))
+
+    def getVal(self, x, y):
+        print(self)
+        # Create a new generator for all possible string combinations at length
+        verticalPerms = itertools.permutations(list(self.characterSet), len(self.vertClues))
+        horizontalPerms = itertools.permutations(list(self.characterSet), len(self.horizClues))
             
-# Parse strings for horiz and vertical
-horizClues = [ tag.contents[0] for soup in horizClueSoup for tag in soup.contents if type(tag) == Tag and len(tag.contents) > 0 ]
-vertClues = [ tag.contents[0] for soup in vertClueSoup for tag in soup.contents if type(tag) == Tag and len(tag.contents) > 0 ]
+        # Compile the regex for this square
+        hre = re.compile(self.horizClues[x])
+        vre = re.compile(self.vertClues[y])
 
-print(horizClues)
-print(vertClues)
+        hp, _ = itertools.tee(horizontalPerms)
+        vp, vp_b = itertools.tee(verticalPerms)     
+                
+        # For every possible combination of strings in vertical and horizontal perms
+        for hstr in hp:
+            vp, vp_b = itertools.tee(vp)
+            for vstr in vp_b:
+                if (hre.match("".join(list(hstr))) and vre.match("".join(list(vstr)))):
+                    retVal = "Solution = " + str(list(hstr)[x])
+                    return retVal
+              
+s = Solver()
+s.solve()
