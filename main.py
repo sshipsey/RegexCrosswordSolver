87e6ca2215989a12fd4a3dfba967afe300c6658a
Steve Shipsey <sashipsey@gmail.com>
2016-01-31 21:39:48 -0500

    Some commenting.

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ from bs4.element import Tag
 import itertools
 import re
 
+# Python solver for RegexCrossword
+# https://regexcrossword.com
 class Solver:
-    site = 'https://regexcrossword.com/challenges/beginner/puzzles/1'
+    
+    # Default constructor
+    def __init__(puzzleURL):
+        self.puzzleURL = puzzleURL
+    
+    # Valid character set for string testing against puzzle regex
     characterSet = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
 
+    # Create a browser instance and scrape the puzzle from the given URL
     browser = webdriver.PhantomJS()
-    browser.get(site)
+    browser.get(puzzeURL)
     pageSoup = BeautifulSoup(browser.page_source, "html.parser")
+    
+    # Find our vertical and horizontal clues and store them in lists
     vertClueSoup = pageSoup.find_all(re.compile('th'), class_ = "clue")
     horizClueSoup = pageSoup.find_all(re.compile('div'), class_ = "clue")
 
@@ -18,17 +28,21 @@ class Solver:
     horizClues = [ tag.contents[0] for soup in horizClueSoup for tag in soup.contents if type(tag) == Tag and len(tag.contents) > 0 ]
     vertClues = [ tag.contents[0] for soup in vertClueSoup for tag in soup.contents if type(tag) == Tag and len(tag.contents) > 0 ]
 
+    # Method to solve an entire puzzle
     def solve(self):
+    
         # For every square in the grid
         for y, vc in enumerate(self.vertClues):
             for x, hc in enumerate(self.horizClues):
                 print(self.getVal(x, y))
 
+    # Method to solve the character for a x,y location
     def getVal(self, x, y):
+        
         # Create a new generator for all possible string combinations at length
         verticalPerms = itertools.permutations(list(self.characterSet), len(self.vertClues))
         horizontalPerms = itertools.permutations(list(self.characterSet), len(self.horizClues))
-            
+        
         # Compile the regex for this square
         hre = re.compile('^' + self.horizClues[x] + '$')
         vre = re.compile('^' + self.vertClues[y] + '$')
@@ -47,9 +61,9 @@ class Solver:
                     retVal = "Solution (" + str(x) + "," + str(y) + ") = " + str(list(hstr)[x])
                     return retVal
                     
-        # This should never happen, no intersection found
+        # This should never happen for a valid puzzle, no intersection found
         return "Solution (" + str(x) + "," + str(y) + ") Not found"
 
 # Create a solver and solve
-s = Solver()
+s = Solver('https://regexcrossword.com/challenges/tutorial/puzzles/4')
 s.solve()
